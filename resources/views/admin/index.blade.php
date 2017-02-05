7080f85ad30e5329b302ebee0d0310896c4635af
bigrocs <bigrocs@qq.com>
2017-02-05 13:51:59 +0800

    修正首页错误

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>LaravelCmf</title>
     <link rel="shortcut icon" href="{{ config('website.icon') }}">
-    <link rel="stylesheet" href="{{ asset('/css/app.css') }}"> -->
+    <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
     <!--[if lt IE 9]>
         <script src="http://cdn.bootcss.com/html5shiv/3.7.2/html5shiv.min.js"></script>
         <script src="http://cdn.bootcss.com/respond.js/1.4.2/respond.min.js"></script>
