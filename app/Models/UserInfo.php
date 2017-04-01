44ba56c66f2aa9a1c06a629a2f1e58ebde4b1b93
bigrocs <bigrocs@qq.com>
2017-04-01 13:53:47 +0800

    根据ID获取头像图片URL 增加访问器

--- app/Models/UserInfo.php
+++ app/Models/UserInfo.php
@@ -1,10 +1,26 @@
 <?php
 
 namespace App\Models;
-
+use App\CustomCollection;
 use Illuminate\Database\Eloquent\Model;
 
+use Facades\ {
+    App\Facades\Helpers
+};
 class UserInfo extends Model
 {
-    //
+
+	/**
+	 * [getAvatarAttribute 根据ID获取头像图片URL]
+	 * @author BigRocs
+	 * @email    bigrocs@qq.com
+	 * @DateTime 2017-04-01T13:51:44+0800
+	 * @param    [type]                   $value [头像图片ID]
+	 * @return   [type]                          [description]
+	 */
+    public function getAvatarAttribute($id)
+    {
+    	$uploadObject = Helpers::getUploadWhereFirst($id);
+        return $uploadObject->url;
+    }
 }
