01e8658d501e509c813b1cc099f632ac2ee51497
bigrocs <bigrocs@qq.com>
2017-04-16 08:57:44 +0800

    增加自定义字段

--- app/Models/AdminConfig.php
+++ app/Models/AdminConfig.php
@@ -1,8 +1,12 @@
 <?php
 
 namespace App\Models;
-
+use App\CustomCollection;
 use Illuminate\Database\Eloquent\Model;
+
+use Facades\ {
+    App\Facades\Helpers
+};
 /**
  * Class AdminConfig
  * @package App\Models
@@ -36,6 +40,44 @@ class AdminConfig extends Model
         'title' => 'required|string',
         'name' => 'required|string',
         'value' => 'required|string'
-    ];
-
+    ]
+    ;
+    protected $appends = ['imageUrl','uploadUrl','label','rows'];
+    /**
+     * [getImageUrlAttribute 获取上传图片网址]
+     * @author BigRocs
+     * @email    bigrocs@qq.com
+     * @DateTime 2017-04-16T08:40:27+0800
+     * @return   [type]                   [description]
+     */
+    public function getImageUrlAttribute()
+    {
+        if ($this->attributes['type'] == 'picture') {
+            $uploadObject = Helpers::getUploadWhereFirst($this->attributes['value']);
+            return $uploadObject->url;
+        }
+    }
+    /**
+     * [getUploadUrlAttribute 获取上传文件路径]
+     * @author BigRocs
+     * @email    bigrocs@qq.com
+     * @DateTime 2017-04-16T08:40:42+0800
+     * @return   [type]                   [description]
+     */
+    public function getUploadUrlAttribute()
+    {
+        if ($this->attributes['type'] == 'picture') {
+            return '/api/admin/system/upload/image';
+        }
+    }
+    public function getLabelAttribute()
+    {
+        return $this->attributes['title'];
+    }
+    public function getRowsAttribute()
+    {
+        if ($this->attributes['type'] == 'textarea') {
+            return 5;
+        }
+    }
 }
