fa3dcfd6721007b154f6d1999064d65e49c06c7c
bigrocs <bigrocs@qq.com>
2017-04-18 13:45:34 +0800

    私有化roleModel

--- app/Http/Controllers/Api/Admin/UserController.php
+++ app/Http/Controllers/Api/Admin/UserController.php
@@ -17,6 +17,7 @@ class UserController extends Controller
 {
 	/** @var userRepo */
     private $userModel;
+    private $roleModel;
 
     public function __construct(User $userRepo,Role $rolePepo)
     {
