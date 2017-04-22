07280cb602faab1c91c3186367cca9b325561be5
bigrocs <bigrocs@qq.com>
2017-04-22 10:02:03 +0800

    增加角色操作方法

--- app/Http/Controllers/Api/Admin/RoleController.php
+++ app/Http/Controllers/Api/Admin/RoleController.php
@@ -24,21 +24,22 @@ class RoleController extends Controller
         list($group,$pageSizes,$pageSize,$page,$selectSearch,$inputSearch) = Helpers::compileTableRequest($request);
         // [$total 获取数据总数]
         $total = $this->roleModel
-                        ->where('status', '>=', 0)
+                        // ->where('status', '>=', 0)
                         ->where($selectSearch, 'like', $inputSearch)
                         ->count();
         //[$userModel 获取数据对象]
         $roles = $this->roleModel
                         ->page($page, $pageSize)
                         ->orderBy('id', 'ASC')
-                        ->where('status', '>=', 0)
+                        // ->where('status', '>=', 0)
                         ->where($selectSearch, 'like', $inputSearch)
                         ->get();
     
-       return $data = BuilderData::addTableData($roles)
+        return $data = BuilderData::addTableData($roles)
                                 ->addTableColumn(['prop' => 'id',         'label'=> 'ID',     'width'=> 55])
-                                ->addTableColumn(['prop' => 'name',       'label'=> '角色名', 'minWidth'=> 120])
+                                ->addTableColumn(['prop' => 'name',       'label'=> '角色标识', 'minWidth'=> 120])
                                 ->addTableColumn(['prop' => 'display_name','label'=> '角色名称','minWidth'=> 180])
+                                ->addTableColumn(['prop' => 'description','label'=> '角色描述','minWidth'=> 280])
                                 ->addTableColumn(['prop' => 'rightButton','label'=> '操作',   'minWidth'=> 220,  'type' => 'btn'])
                                 ->addTableApiUrl('delete','/api/admin/system/role/delete')         //添加删除通信API
                                 ->addTableApiUrl('add','/api/admin/system/role/add')               //添加数据接口
@@ -52,5 +53,66 @@ class RoleController extends Controller
                                 ->setSearchSelect(['id'=>'ID','name'=>'用户名','email'=>'邮箱','mobile'=>'手机'])
                                 ->setTitle('配置管理')
                                 ->get();
+    }
+    public function delete(Request $request){
+        $input = $request->all();
+        foreach ($input as $id => $value) {
+            $response = $this->roleModel->find($id)->forceDelete();
+        }
+        $data = [
+                    'title'     => '删除成功',
+                    'message'   => '角色数据删除成功!',
+                    'type'      => 'success',
+                ];
+        return response()->json($data, 200);
+    }
+    public function add(){
+        return $data = BuilderData::addFormApiUrl('submit','/api/admin/system/role/store')               //添加Submit通信API
+                            ->setFormTitle('新增角色')                                                   //添form表单页面标题
+                            ->setFormConfig(['width'=>'90px'])
+                            ->addFormItem(['name' => 'name',      'type' => 'text',     'label' => '角色标识'     ])
+                            ->addFormItem(['name' => 'display_name','type' => 'text',     'label' => '角色名称'   ])
+                            ->addFormItem(['name' => 'description','type' => 'textarea','label' => '角色描述'   ])
+                            ->setFormRules($this->roleModel->getRules())
+                            ->get();
+    }
+    public function store(Request $request)
+    {
+        $roleModel = new Role();
+        $roleModel->name = $request->name;
+        $roleModel->display_name = $request->display_name;
+        $roleModel->description = $request->description;
+        $roleModel->save();
+        $data = [
+                        'title'     => '新增角色成功！',
+                        'message'   => '新增角色数据成功！!',
+                        'type'      => 'success',
+                ];
+
+        return response()->json($data, 200);
+    }
+    public function edit(Request $request){ 
+        $roles = $this->roleModel->find($request->id);
+        return $data = BuilderData::addFormApiUrl('submit','/api/admin/system/role/update')               //添加Submit通信API
+                            ->setFormTitle('新增角色')                                                   //添form表单页面标题
+                            ->setFormConfig(['width'=>'90px'])
+                            ->addFormItem(['name' => 'id',        'type' => 'hidden',   'label' => 'ID'     ])
+                            ->addFormItem(['name' => 'name',      'type' => 'text',     'label' => '角色标识'     ])
+                            ->addFormItem(['name' => 'display_name','type' => 'text',     'label' => '角色名称'   ])
+                            ->addFormItem(['name' => 'description','type' => 'textarea','label' => '角色描述'   ])
+                            ->setFormObject($roles)
+                            ->setFormRules($this->roleModel->getRules())
+                            ->get();
+    }
+    public function update(Request $request)
+    {
+        $input = $request->all();
+        $role = $this->roleModel->find($input['id'])->fill($input)->save();
+        $data = [
+                        'title'     => '用户编辑成功！',
+                        'message'   => '编辑用户数据成功！!',
+                        'type'      => 'success',
+                    ];
+        return response()->json($data, 200);
     }
 }
--- app/Models/Role.php
+++ app/Models/Role.php
@@ -6,4 +6,26 @@ use Zizaco\Entrust\EntrustRole;
 class Role extends EntrustRole
 {
 	use CommonModel;
+	/**
+     * [getRules 前端验证规则]
+     * @author BigRocs
+     * @email    bigrocs@qq.com
+     * @DateTime 2017-04-14T11:09:32+0800
+     * @return   [type]                   [description]
+     */
+    public function getRules(){
+        $rules = [
+            'name'=> [
+                ['required' => true,  'message' => '请输入角色标识', 'trigger'=> 'blur'],
+                [ 'min'=> 4, 'max'=> 20, 'message'=> '长度在 4 到 20 个英文字符', 'trigger'=> 'blur' ]
+            ],
+            'display_name'=> [
+                [ 'required'=> true, 'message'=> '请输入角色名', 'trigger'=> 'blur' ],
+            ],
+            'description'=> [
+                [ 'required'=> true, 'message'=> '请输入角色描述', 'trigger'=> 'blur' ],
+            ],
+        ];
+        return $rules;
+    }
 }
