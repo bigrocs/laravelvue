c08aff43aae04d5f3f0f5873193846106abd36cf
bigrocs <bigrocs@qq.com>
2017-05-05 15:45:48 +0800

    修复store计算属性无法直接绑定 组件改为$store直接绑定

--- resources/assets/js/components/builder/packages/search.vue
+++ resources/assets/js/components/builder/packages/search.vue
@@ -1,12 +1,12 @@
 <template>
     <el-input
         v-if="datas.title"
-        v-model="inputSearch"
+        v-model="$store.state.postData.inputSearch"
         :placeholder="datas.title"
         @keyup.enter.native="handleInputConfirm"
         @blur="handleInputConfirm"
     >
-        <el-select v-if="datas.select" v-model="selectSearch" slot="prepend" placeholder="请选择">
+        <el-select v-if="datas.select" v-model="$store.state.postData.selectSearch" slot="prepend" placeholder="请选择">
             <el-option v-for="(label, value) in datas.select"  :key="value" :label="label" :value="value"></el-option>
         </el-select>
         <el-button slot="append" icon="search" @click="handleInputConfirm"></el-button>
@@ -24,23 +24,9 @@ export default {
     computed: {
         ...mapState({
             postData: 'postData',
-            selectSearch: state => state.postData.selectSearch,
-            inputSearch: state => state.postData.inputSearch,
         }),
     },
-    watch: {
-        selectSearch:'setSelectSearch',
-    },
     methods: {
-        /**
-         * [setSelectSearch 选择搜索数据列]
-         * @author BigRocs
-         * @email    bigrocs@qq.com
-         * @DateTime 2017-02-16T15:47:29+0800
-         */
-        setSelectSearch(){
-            this.$store.dispatch('setPostData',{selectSearch:this.selectSearch})//设置搜索字段
-        },
         /**
          * [handleInputConfirm 根据搜索获取数据]
          * @author BigRocs
@@ -49,7 +35,6 @@ export default {
          * @return   {[type]}                 [description]
          */
         handleInputConfirm(){
-            this.$store.dispatch('setPostData',{inputSearch:this.inputSearch})//设置搜索内容
             this.$store.dispatch('getCurrentData',this.postData)//根据inputSearch获取数据
         }
     }
